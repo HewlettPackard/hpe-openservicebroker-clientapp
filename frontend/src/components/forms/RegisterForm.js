eedf795cf625bcb0b9c0424048d0fab5ff31e555
Carson Stone <carson.stone@hpe.com>
2019-07-09 13:43:56 -0600

    re-added lost register form changes

--- frontend/src/components/forms/RegisterForm.js
+++ frontend/src/components/forms/RegisterForm.js
@@ -5,35 +5,86 @@ import axios from "axios";
 
 //========================================= Register Form
 class RegisterForm extends Component {
-  render() {
-    return (
-      <Box  align='center' justify="start" pad="medium" flex>
-        <Heading size="medium" level="2">
-          Register service
-        </Heading>
-        <Form>
-          <FormField label="Name" help="Name of the service">
-            <TextInput />
-          </FormField>
-          <FormField
-            label="URL"
-            help="Use https for secure connection. Ex: https://127.0.0.1:7009"
-          >
-            <TextInput />
-          </FormField>
-          <FormField label="Username" help="Username used to login to broker">
-            <TextInput />
-          </FormField>
-          <FormField label="Password">
-            <TextInput type='password'/>
-          </FormField>
-          <Link to='/catalog'>
-            <Button label="Submit" margin={{ top: 'medium', left: '35%' }} />
-          </Link>
-        </Form>
-      </Box>
-    );
-  }
+	state = {
+		sname: "",
+		url: "",
+		uname: "",
+		pwd: ""
+	};
+
+	handleChange = e => {
+		this.setState({
+			[e.target.id]: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		let data = {
+			sname: this.state.sname,
+			url: this.state.url,
+			uname: this.state.uname,
+			pwd: this.state.pwd
+		};
+
+		axios
+			.post("http://localhost:5000/register", data)
+			.then(response => {
+				console.log(response);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+	render() {
+		return (
+			<Box align="center" justify="start" pad="medium" flex>
+				<Heading size="medium" level="2">
+					Register service
+				</Heading>
+				<Form>
+					<FormField label="Service name">
+						<TextInput
+							id="sname"
+							value={this.state.sname}
+							onChange={this.handleChange}
+						/>
+					</FormField>
+					<FormField
+						label="URL"
+						help="Use https for secure connection. Ex: https://127.0.0.1:7009"
+					>
+						<TextInput
+							id="url"
+							value={this.state.url}
+							onChange={this.handleChange}
+						/>
+					</FormField>
+					<FormField label="Username">
+						<TextInput
+							id="uname"
+							placeholder="Username used to login to broker"
+							value={this.state.uname}
+							onChange={this.handleChange}
+						/>
+					</FormField>
+					<FormField label="Password">
+						<TextInput
+							id="pwd"
+							value={this.state.pwd}
+							onChange={this.handleChange}
+						/>
+					</FormField>
+					<Link to="/catalog">
+						<Button
+							label="Submit"
+							margin={{ top: "medium", left: "35%" }}
+							onClick={this.handleSubmit}
+						/>
+					</Link>
+				</Form>
+			</Box>
+		);
+	}
 }
 
 export default RegisterForm;
