15ff6273fbaba1616f88c925a3a1eba8023bdb25
Carson Stone <carson.stone@hpe.com>
2019-07-11 11:16:53 -0600

    added empty catalog message

--- frontend/src/components/catalog-results/CatalogResults.js
+++ frontend/src/components/catalog-results/CatalogResults.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Box, Button, Grid, TextInput } from "grommet";
+import { Box, Button, Grid, Text, TextInput } from "grommet";
 import { Sync } from "grommet-icons";
 import Card from "../card/Card";
 import axios from "axios";
@@ -70,48 +70,63 @@ class CatalogResults extends Component {
 	};
 
 	render() {
-		const { deployedList, showList, value } = this.state;
+    const { deployedList, serviceList, showList, value } = this.state;
+    let showEmptyMessage = false;
+    if (serviceList.length === 0) 
+      showEmptyMessage = true;
 
 		return (
 			<Box fill>
-				<Box fill="horizontal" flex={false}>
-					<Box
-						width="70%"
-						alignSelf="center"
-						margin={{ bottom: "large" }}
-						border={{ size: "xsmall", color: "light-5" }}
-					>
-						<TextInput
-							placeholder="search"
-							value={value}
-							onChange={this.setValue}
-						/>
-					</Box>
-				</Box>
-				<Grid gap="large" columns="small" rows="small">
-					{showList.map(service => (
-						<Card service={service} key={service.name} />
-					))}
-				</Grid>
-				<Box
-					width="medium"
-					align="center"
-					justify="center"
-					alignSelf="center"
-					margin={{ top: "30px" }}
-				>
-					<Button
-						fill
-						color="brand"
-						label="Update Catalog"
-						gap="small"
-						icon={<Sync size="medium" color="brand" />}
-						onClick={() => {
-							alert("Udating Services");
-							this.update();
-						}}
-					/>
-				</Box>
+				
+        { showEmptyMessage && (
+            <Box className='emptyMessage' align='center'>
+              <Text size='xlarge' color='brand'>You do not have any brokers registered. Register a broker to access services.</Text>
+            </Box>
+          )
+        }
+        { !showEmptyMessage && (
+            <Box>
+              <Box fill="horizontal" flex={false}>
+                <Box
+                  width="70%"
+                  alignSelf="center"
+                  margin={{ bottom: "large" }}
+                  border={{ size: "xsmall", color: "light-5" }}
+                >
+                  <TextInput
+                    placeholder="search"
+                    value={value}
+                    onChange={this.setValue}
+                  />
+                </Box>
+              </Box>
+              <Grid gap="large" columns="small" rows="small">
+                {showList.map(service => (
+                  <Card service={service} key={service.name} />
+                ))}
+              </Grid>
+              <Box
+                width="medium"
+                align="center"
+                justify="center"
+                alignSelf="center"
+                margin={{ top: "30px" }}
+              >
+                <Button
+                  fill
+                  color="brand"
+                  label="Update Catalog"
+                  gap="small"
+                  icon={<Sync size="medium" color="brand" />}
+                  onClick={() => {
+                    alert("Udating Services");
+                    this.update();
+                  }}
+                />
+              </Box>
+            </Box>
+          )   
+        }
 			</Box>
 		);
 	}
